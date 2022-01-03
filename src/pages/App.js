07490b1550f01563f1648b52507c19e4061f6182
Taichen <jatr812@gmail.com>
2022-01-02 17:48:03 -0800

    Fixed import img

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,10 @@
-import logo from '../../public/logo.svg';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
