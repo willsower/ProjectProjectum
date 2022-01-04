ad1cdee27bd0ee35a69b7837e4d28e722c33c401
Taichen <jatr812@gmail.com>
2022-01-03 17:52:03 -0800

    Working on making square white

---
+++ src/components/settings.js
@@ -0,0 +1,11 @@
+export default function Settings({setSettingsOpen}) {
+  return (
+  <div className = "fixed top-0 left-0 w-screen h-screen bg-black opacity-50 ">
+      hi
+      {/* Inside white */}
+      <div className = "bg-red-500 z-50 w-96 h-3/4 absolute top-1/2 left-1/2 translate-x-1/2 translate-y-1/2">
+            heyoooo
+      </div>
+  </div>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,18 @@
 import Header from "../components/header";
+import LeftNav from "../components/menu";
+import Settings from "../components/settings";
 
 function Home() {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-    </>
+      <Settings />      
+
+      <div className="flex grow">
+        <LeftNav />
+        {/* <Board /> */}
+      </div>
+    </div>
   );
 }
 
