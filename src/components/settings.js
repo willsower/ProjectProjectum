48af5c8d43bfad7e1518f7a6a38ed0183dcb6120
Taichen <jatr812@gmail.com>
2022-01-04 16:01:40 -0800

    Adding custom CSS file for settings

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,14 +1,23 @@
+import "../styles/settings.css";
+
 export default function Settings({ setSettingsOpen }) {
   return (
-    <div className = "absolute w-screen h-screen">
+    <div className="absolute w-screen h-screen">
       {/* Gray Background */}
-      <div className="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-20">
-
-      </div>
+      <div className="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-20"></div>
 
       {/* Inside white */}
-      <div className="bg-white z-20 max-w-xl h-96 relative m-auto mt-12 rounded">
-        heyoooo
+      <div className="bg-white z-20 max-w-xl h-96 relative m-auto mt-12 rounded-xl flex">
+        {/* Settings Nav */}
+        <div className="w-36 bg-red-200">
+          <p className = "settingsName">Account Settings</p>
+          <p className = "settingsName">Appearance</p>
+          <p className = "settingsName">Preferences</p>
+          <p className = "settingsName">Menu Options</p>
+        </div>
+
+        {/* Output */}
+        <div className="bg-blue-200 grow"></div>
       </div>
     </div>
   );
